2f511cf0e4d393f3d1eb6cd5fb5ed5fa6b698e5b
Vladislav Smirnov <obivaniche@yandex.ru>
2025-02-14 01:01:11 +0300

    Add new page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const popup = document.querySelectorAll('.popup');
 const popupContact = document.querySelector('.popup-contact');
 const buttonContacts = document.querySelectorAll('.button-contact');
 
-// Находим кнопки отрытия попапа
+// Находим кнопки отрытия попапа и открываем его
 if (buttonContacts.length > 0) {
     buttonContacts.forEach((buttonContact) => {
         buttonContact.addEventListener('click', onButtonContact);
@@ -52,17 +52,21 @@ popup.forEach((popup) =>
 // Форма
 // Находим форму
 const form = document.querySelector('.form');
+// Находим кнопку отправки и отключаем ее
+const button = form.querySelector('.button_form');
+button.classList.add('form__submit-button_disabled');
 
 // Очищаем форму
 form.addEventListener('submit', (evt) => {
     evt.target.reset();
 });
 
-// Отправка формы
+// Сериализируем поля формы
 function serializeForm(formNode) {
     return new FormData(formNode);
 };
 
+// Отправка данных
 async function sendData(data) {
     return await fetch('/api/apply/', {
         method: 'POST',
@@ -71,11 +75,50 @@ async function sendData(data) {
     });
 };
 
+// Успех
+function onSuccess() {
+    alert('Ваша заявка отправлена!');
+};
+
+// Неуспех
+function onError() {
+    alert('Ваша заявка отклонена!');
+};
+
+// Лоадер и сообщения
 async function handleFormSubmit(evt) {
     evt.preventDefault();
-  
+    toggleLoader();
     const data = serializeForm(evt.target);
-    const response = await sendData(data);
-  }
+    const { status } = await sendData(data);
+    toggleLoader();
 
+    if (status === 200) {
+        onSuccess(evt.target);
+    } else {
+        onError(evt.target);
+    };
+};
+
+// Слушатель отправки формы
 form.addEventListener('submit', handleFormSubmit);
+
+// Активация лоадера
+function toggleLoader() {
+    const loader = document.querySelector('.form__loader');
+    loader.classList.toggle('form__loader_active');
+};
+
+// Включение кнопки отправки при валидации
+function checkValidity(evt) {
+    const formNode = evt.target.form;
+    const isValid = formNode.checkValidity();
+
+    if (isValid) {
+        button.classList.remove('form__submit-button_disabled');
+        button.classList.add('form__submit-button_active');
+    };
+};
+
+// Слушатель кнопки отправки
+form.addEventListener('input', checkValidity);
